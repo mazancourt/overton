569fb5596944f20272c35c371ffc5d27a3e8e7dd
mazancourt <hugues@mazancourt.com>
2021-10-14 19:28:48 +0200

    chore: refactoring

--- src/main/scripts/create-corpus.py
+++ src/main/scripts/create-corpus.py
@@ -2,23 +2,24 @@
 from pathlib import Path
 from tqdm import tqdm
 from elasticsearch_dsl import Q
+from dotenv import load_dotenv
 from politexts.elasticsearch import Polindex, Speech
 from politexts.nlp import Nlp, text_to_id
-from dotenv import load_dotenv
 
 load_dotenv()
 
-user = u"Emmanuel Macron "
+USER = u"Emmanuel Macron "
 
 Polindex.connect()
 s = Speech.search(using=Polindex.es, index="speech-vie-publique")
-s = s.query("nested", path="persons", query=Q('term', persons__name=user)).filter("range", published={"gt": "now-1M"})
+s = s.query("nested", path="persons", query=Q('term', persons__name=USER)).\
+    filter("range", published={"gt": "now-1M"})
 s = s.doc_type(Speech)
 results = s.execute()
 
 speakers = set()
 nlp = Nlp()
-root = Path(text_to_id(user))
+root = Path(text_to_id(USER))
 problems = root / Path("problems") / Path("fr")
 problems.mkdir(parents=True, exist_ok=True)
 solutions = root / Path("solutions") / Path("fr")
@@ -26,10 +27,12 @@ solutions.mkdir(parents=True, exist_ok=True)
 
 for speech in results:
     fname = speech.meta.id + ".txt"
-    pb = open(problems / fname, "w")
-    sol = open(solutions / fname, "w")
+    pb = open(problems / fname, "w", encoding="utf8")
+    sol = open(solutions / fname, "w", encoding="utf8")
     speakers.update([p.name for p in speech.persons])
-    for sent in tqdm(nlp.split_sentences(speech.fulltext), unit="sentence", desc=str(speech.published)):
+    for sent in tqdm(nlp.split_sentences(speech.fulltext),
+                     unit="sentence",
+                     desc=str(speech.published)):
         cat = nlp.classify(sent)[0]
         if cat.startswith("Pro"):
             pb.write(sent + "\n")
@@ -38,7 +41,7 @@ for speech in results:
     pb.close()
     sol.close()
 
-with open(root / "stopwords.txt", "w") as stopwords:
+with open(root / "stopwords.txt", "w", encoding="utf8") as stopwords:
     for word in speakers:
         stopwords.write(word)
         stopwords.write("\n")
