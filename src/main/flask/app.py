7903e25c0dc4c9e82791ed460a952a5fb91c1848
mazancourt <hugues@mazancourt.com>
2022-02-09 18:39:13 +0100

    feat: query status for a batch of tasks

--- src/main/flask/app.py
+++ src/main/flask/app.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from flask import Flask, jsonify, request
 from worker.celery import make_celery
 from tasks import enhance_speech
@@ -53,3 +55,32 @@ def task_status(task_id):
     except Exception as err:
         app.logger.error("Error getting task status from worker: task_id=%s - %s", task_id, err)
     return jsonify({"status":task.state if task else "FAIL"})
+
+
+@app.route('/batch_status', methods=['POST'])
+def batch_status():
+    """
+    Same as status, but for a list of IDs.
+    INput message shall be { "ids": [id1, id2, ...] } where ids are task-ids
+    The returned global status will be PENDING unless all tasks are SUCCESSful
+    :return: json message with {"status": global status, "progress": progress-message, "details": counts by status }
+    """
+    speech = request.get_json()
+    status_counts = Counter()
+    task_id_list = speech["ids"]
+    for task_id in task_id_list:
+        try:
+            task = client.AsyncResult(task_id)
+            status = task.state
+        except Exception as err:
+            app.logger.error("Error getting task status from worker: task_id=%s - %s", task_id, err)
+            status = "ERROR"
+        status_counts[status] += 1
+    if status_counts["SUCCESS"] == len(task_id_list):
+        global_status = "SUCCESS"
+    else:
+        global_status = "PENDING"
+    progress = "%.2f%% (%d out of %d)" % (100*status_counts["SUCCESS"]/len(task_id_list),
+                                          status_counts["SUCCESS"], len(task_id_list))
+    details = [{status: status_counts[status] for status in status_counts}]
+    return jsonify({"status": global_status, "progress": progress, "details": details})
